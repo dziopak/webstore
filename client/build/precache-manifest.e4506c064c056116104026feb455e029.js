a3b05169df2a403c5ee0a0b0f5dc654527d4ce7e
dziopak <michal@dziopak.com>
2019-08-30 21:48:41 +0200

    Client moved to it's own directory...

---
+++ client/build/precache-manifest.e4506c064c056116104026feb455e029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a35c5d58a7181a63fb31243bc22734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd2a50a15474b6377f3",
+    "url": "/static/css/main.954e4b64.chunk.css"
+  },
+  {
+    "revision": "524571e25505b46da4e2",
+    "url": "/static/js/2.0b9e2378.chunk.js"
+  },
+  {
+    "revision": "cdd2a50a15474b6377f3",
+    "url": "/static/js/main.b9e8a1c9.chunk.js"
+  },
+  {
+    "revision": "b721ef581ceece9d2d8b",
+    "url": "/static/js/runtime~main.43ea9386.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
